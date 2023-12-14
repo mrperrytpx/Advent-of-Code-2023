6e3f41aa39ffbee9fcf7ab481259db76303486df
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-12-14 18:28:22 +0100

    Advent of Code Day 14 part 2, easily the worst code this year

--- Day14/part2.js
+++ Day14/part2.js
@@ -5,109 +5,206 @@ let file = readFile(__dirname + "/input.txt", "utf-8")
     .filter((_) => _.trim())
     .map((x) => x.split(""));
 
-const CYCLES = 1000000000;
+const CYCLES = 1_000_000_000;
 let load = 0;
 
+let cache = new Map();
+
 for (let c = 0; c < CYCLES; c++) {
-    console.log(c);
-    for (let i = 1; i < file.length; i++) {
-        for (let j = 0; j < file[i].length; j++) {
-            if (file?.[i]?.[j] === "O" && file?.[i - 1]?.[j] === ".") {
-                let above = i - 1;
-                let curr = i;
-                while (file?.[above]?.[j] === ".") {
-                    file[above][j] = "O";
-                    file[curr][j] = ".";
-                    above--;
-                    curr--;
-                }
-            }
-        }
-    }
+    let northRockMap = new Set();
 
-    // console.log("after roll north");
-    // console.log(
-    //     JSON.stringify(
-    //         file.map((x) => x.join("")),
-    //         null,
-    //         2
-    //     )
-    // );
-
-    //west
     for (let i = 0; i < file.length; i++) {
-        for (let j = 1; j < file[i].length; j++) {
-            if (file?.[i]?.[j] === "O" && file?.[i]?.[j - 1] === ".") {
-                let left = j - 1;
-                let curr = j;
-                while (file?.[i]?.[left] === ".") {
-                    file[i][left] = "O";
-                    file[i][curr] = ".";
-                    left--;
-                    curr--;
+        for (let j = 0; j < file[i].length; j++) {
+            if (file?.[i]?.[j] === "#") {
+                northRockMap.add([i, j].toString());
+            }
+        }
+    }
+
+    for (let i = 0; i < file.length; i++) {
+        for (let j = 0; j < file[i].length; j++) {
+            if (i === 0 && file[i][j] === "O") {
+                northRockMap.add([i, j].toString());
+            } else {
+                if (
+                    file[i][j] === "O" &&
+                    (file[i - 1][j] === "O" || file[i - 1][j] === "#")
+                ) {
+                    northRockMap.add([i, j].toString());
+                    continue;
+                }
+                if (file[i][j] === "O" && file[i - 1][j] === ".") {
+                    const rocksAbove = [...northRockMap.values()]
+                        .map((x) => x.split(",").map(Number))
+                        .filter((x) => x[1] === j && x[0] <= i);
+                    if (!rocksAbove.length) {
+                        file[0][j] = "O";
+                        file[i][j] = ".";
+                        northRockMap.add([0, j].toString());
+                    } else {
+                        let closestRock = rocksAbove.sort((a, b) => {
+                            if (a[0] === b[0]) return a[1] - b[1];
+                            return a[0] - b[0];
+                        })[rocksAbove.length - 1];
+                        let [iRock, jRock] = [closestRock[0] + 1, j];
+                        file[iRock][jRock] = "O";
+                        file[i][j] = ".";
+                        northRockMap.add([iRock, jRock].toString());
+                    }
                 }
             }
         }
     }
 
-    // console.log("after roll west");
-    // console.log(
-    //     JSON.stringify(
-    //         file.map((x) => x.join("")),
-    //         null,
-    //         2
-    //     )
-    // );
+    // west
+
+    let westRockMap = new Set();
+
+    for (let i = 0; i < file.length; i++) {
+        for (let j = 0; j < file[i].length; j++) {
+            if (file?.[i]?.[j] === "#") {
+                westRockMap.add([i, j].toString());
+            }
+        }
+    }
+
+    for (let i = 0; i < file.length; i++) {
+        for (let j = 0; j < file[i].length; j++) {
+            if (j === 0 && file[i][j] === "O") {
+                westRockMap.add([i, j].toString());
+            } else {
+                if (
+                    file[i][j] === "O" &&
+                    (file[i][j - 1] === "O" || file[i][j - 1] === "#")
+                ) {
+                    westRockMap.add([i, j].toString());
+                    continue;
+                }
+                if (file[i][j] === "O" && file[i][j - 1] === ".") {
+                    const rocksLeft = [...westRockMap.values()]
+                        .map((x) => x.split(",").map(Number))
+                        .filter((x) => x[0] === i && x[1] <= j);
+
+                    if (!rocksLeft.length) {
+                        file[i][0] = "O";
+                        file[i][j] = ".";
+                        westRockMap.add([i, 0].toString());
+                    } else {
+                        let closestRock = rocksLeft.sort((a, b) => {
+                            if (a[0] === b[0]) return a[1] - b[1];
+                            return a[0] - b[0];
+                        })[rocksLeft.length - 1];
+                        let [iRock, jRock] = [i, closestRock[1] + 1];
+                        file[iRock][jRock] = "O";
+                        file[i][j] = ".";
+                        westRockMap.add([iRock, jRock].toString());
+                    }
+                }
+            }
+        }
+    }
 
     //south
+    let southRockMap = new Set();
+
+    for (let i = 0; i < file.length; i++) {
+        for (let j = 0; j < file[i].length; j++) {
+            if (file?.[i]?.[j] === "#") {
+                southRockMap.add([i, j].toString());
+            }
+        }
+    }
+
     for (let i = file.length - 1; i >= 0; i--) {
         for (let j = 0; j < file[i].length; j++) {
-            if (file?.[i]?.[j] === "O" && file?.[i + 1]?.[j] === ".") {
-                let below = i + 1;
-                let curr = i;
-                while (file?.[below]?.[j] === ".") {
-                    file[below][j] = "O";
-                    file[curr][j] = ".";
-                    below++;
-                    curr++;
+            if (i === file.length - 1 && file[i][j] === "O") {
+                southRockMap.add([i, j].toString());
+            } else {
+                if (
+                    file[i][j] === "O" &&
+                    (file[i + 1][j] === "O" || file[i + 1][j] === "#")
+                ) {
+                    southRockMap.add([i, j].toString());
+                    continue;
+                }
+                if (file[i][j] === "O" && file[i + 1][j] === ".") {
+                    const rocksBelow = [...southRockMap.values()]
+                        .map((x) => x.split(",").map(Number))
+                        .filter((x) => x[1] === j && x[0] >= i);
+
+                    if (!rocksBelow.length) {
+                        file[file.length - 1][j] = "O";
+                        file[i][j] = ".";
+                        southRockMap.add([file.length - 1, j].toString());
+                    } else {
+                        let closestRock = rocksBelow.sort((a, b) => {
+                            if (a[0] === b[0]) return a[1] - b[1];
+                            return a[0] - b[0];
+                        })[0];
+
+                        let [iRock, jRock] = [closestRock[0] - 1, j];
+                        file[iRock][jRock] = "O";
+                        file[i][j] = ".";
+                        southRockMap.add([iRock, jRock].toString());
+                    }
                 }
             }
         }
     }
 
-    // console.log("after roll south");
-    // console.log(
-    //     JSON.stringify(
-    //         file.map((x) => x.join("")),
-    //         null,
-    //         2
-    //     )
-    // );
+    // east;
+    let eastRockMap = new Set();
 
-    //east
+    for (let i = 0; i < file.length; i++) {
+        for (let j = 0; j < file[i].length; j++) {
+            if (file?.[i]?.[j] === "#") {
+                eastRockMap.add([i, j].toString());
+            }
+        }
+    }
+
     for (let i = 0; i < file.length; i++) {
         for (let j = file[i].length - 1; j >= 0; j--) {
-            if (file?.[i]?.[j] === "O" && file?.[i]?.[j + 1] === ".") {
-                let right = j + 1;
-                let curr = j;
-                while (file?.[i]?.[right] === ".") {
-                    file[i][right] = "O";
-                    file[i][curr] = ".";
-                    right++;
-                    curr++;
+            if (j === file[i].length - 1 && file[i][j] === "O") {
+                eastRockMap.add([i, j].toString());
+            } else {
+                if (
+                    file[i][j] === "O" &&
+                    (file[i][j + 1] === "O" || file[i][j + 1] === "#")
+                ) {
+                    eastRockMap.add([i, j].toString());
+                    continue;
+                }
+                if (file[i][j] === "O" && file[i][j + 1] === ".") {
+                    const rocksRight = [...eastRockMap.values()]
+                        .map((x) => x.split(",").map(Number))
+                        .filter((x) => x[0] === i && x[1] >= j);
+
+                    if (!rocksRight.length) {
+                        file[i][file[i].length - 1] = "O";
+                        file[i][j] = ".";
+                        eastRockMap.add([i, file[i].length - 1].toString());
+                    } else {
+                        let closestRock = rocksRight.sort((a, b) => {
+                            if (a[0] === b[0]) return a[1] - b[1];
+                            return a[0] - b[0];
+                        })[0];
+                        let [iRock, jRock] = [i, closestRock[1] - 1];
+                        file[iRock][jRock] = "O";
+                        file[i][j] = ".";
+                        eastRockMap.add([iRock, jRock].toString());
+                    }
                 }
             }
         }
     }
 
-    // console.log("after roll east");
-    //     console.log(
-    //         JSON.stringify(
-    //             file.map((x) => x.join("")),
-    //             null,
-    //             2
-    //         )
-    //     );
+    let stringFile = file.map((x) => x.join("")).join("|");
+    if (cache.has(stringFile)) {
+        c = CYCLES - ((CYCLES - c) % (c - cache.get(stringFile)));
+    } else {
+        cache.set(stringFile, c);
+    }
 }
 
 for (let i = 0; i < file.length; i++) {
